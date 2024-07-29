497038b06953275c98501917e7cd0ef11955faa1
Tofu <adrianedelen@gmail.com>
2024-07-29 10:18:06 -0400

    fixed bug with post scraper not handling pagination correctly

--- blogger_scraper/posts.py
+++ blogger_scraper/posts.py
@@ -4,22 +4,27 @@ def get_all_posts(blog_id, api_key):
     posts = []
     next_page_token = ''
     url = f'https://www.googleapis.com/blogger/v3/blogs/{blog_id}/posts'
+    print(f'-------------------------------------------------------------')
+    print(f'Attempting to scrape: {blog_id}')
     while True:
-        url = f'{url}?key={api_key}&maxResults=100'
+        full_url = f'{url}?key={api_key}&maxResults=100'
         if next_page_token:
-            url += f'&pageToken={next_page_token}'
-        response = requests.get(url)
+            print(f'Next Page Token: {next_page_token}')
+            full_url += f'&pageToken={next_page_token}'
+        response = requests.get(full_url)
         if response.status_code == 200:
             data = response.json()
             posts.extend(data['items'])
             next_page_token = data.get('nextPageToken', '')
             if not next_page_token:
+                print(f'no next page token, finishing up.')
                 break
         else:
             print(f'Failed to retrieve posts: {response.status_code}')
             break
+    print(f'total posts obtained: {len(posts)}')
     return posts
 
-def save_posts_to_json(posts, filename='blog_posts.json'):
+def save_posts_to_json(posts, filename='data/blog_posts.json'):
     with open(filename, 'w', encoding='utf-8') as file:
         json.dump(posts, file, ensure_ascii=False, indent=4)
